bc3a2f0191691dd9545931e5775d230328c8c2a1
kyretof <106271210+kyretof@users.noreply.github.com>
2022-08-06 23:55:30 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -17,9 +17,6 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-#Network Manager
-systemctl start NetworkManager
-systemctl enable NetworkManager
 
 #User
 useradd -m kyretof
