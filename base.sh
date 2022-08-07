8e6ea967a4dcaccab5524f0b178a1aa11279572f
kyretof <106271210+kyretof@users.noreply.github.com>
2022-08-07 10:12:15 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -21,20 +21,3 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 #User
 useradd -m kyretof
 usermod -aG wheel,audio,video,optical,storage kyretof
-
-#Font
-pacman -S dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
-
-#Firewall
-sudo pacman -S ufw
-sudo systemctl start ufw
-sudo systemctl enable ufw
-
-#Config
-
-#xinitrc 
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-
-#yay
-cd /opt
-sudo git clone https://aur.archlinux.org/yay-git.git
